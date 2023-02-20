b1f60ba47ec4842bdf6b9ffb455ae7dd56ebe0ef
kirtan111 <kirtanthummar7103@protonmail.com>
2023-02-20 14:17:15 +0530

    20 feb

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Person.css";
+import Aux from "../../../hoc/Auxiliary";
 
 const person = (props) => {
     console.log(" [Person.js] Rendering ");
     return (
-        <div className="Person">
+        <Aux className="Person">
             <p onClick={props.click}>
                 Hello, my name is {props.name} and i am {props.age} year old!{" "}
             </p>
             {/* <p> {props.children} </p> */}
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </Aux>
     );
 };
 export default person;
