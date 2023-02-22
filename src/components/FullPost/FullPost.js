ea61f505bb5100a1ed315e0f644009e48d284a3d
kirtan111 <kirtanthummar7103@protonmail.com>
2023-02-22 12:14:47 +0530

    22 feb

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,14 +1,28 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./FullPost.css";
 
 const FullPost = (props) => {
+    const [fullPost, setFullPost] = useState({ loadedPost: null });
+
+    useEffect(() => {
+        if (props.id) {
+            axios.get("https://jsonplaceholder.typicode.com/posts/" + props.id).then((response) => {
+                // console.log(response);
+                setFullPost({ loadedPost: response.data });
+            });
+        }
+    }, [props.id]);
+
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (props.id) {
+        <p style={{ textAlign: "center" }}>Loading ...</p>;
+    }
+    if (fullPost.loadedPost) {
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{fullPost.loadedPost.title}</h1>
+                <p>{fullPost.loadedPost.content}</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
                 </div>
